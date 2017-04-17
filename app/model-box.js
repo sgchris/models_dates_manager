5d04943ae5aaf7e419962a25ffe7caf253255521
Gregory Chris <sgchris@gmail.com>
2017-04-17 13:42:11 +0300

    implemented (partially) model box directive

--- app/model-box.js
+++ app/model-box.js
@@ -1,8 +1,29 @@
 webApp.directive('modelBox', [function() {
 	return {
-		require: 'ngModel',
-		link: function(scope, element, attributes, ngModel) {
-			console.log('ngModel', ngModel);
+		//require: 'ngModel',
+		replace: true,
+		scope: {
+			model: '=ngModel'
+		},
+		link: function(scope, element, attributes) {
+			scope.defaultImage = 'images/model_silhouette.png';
+			
+			/**
+			 * move the model to the top/bottom of the list
+			 * @param int modelId
+			 * @param string newLocation - "top" / "bottom"
+			 */
+			scope.relocateModel = function(modelId, newLocation) {
+				
+			};
+			
+			/**
+			 * delete the model
+			 * @param int modelId
+			 */
+			scope.deleteModel = function(modelId) {
+				
+			};
 		},
 		templateUrl: 'views/directives/model-box.html',
 	}
